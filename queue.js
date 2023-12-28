88c71cb9c02036868e42ee3307b30a63b599d9da
junyoung <no2jfamily@tukorea.ac.kr>
2023-12-28 14:09:21 +0900

    chore: 큐 기능 수정

--- queue.js
+++ queue.js
@@ -1,29 +1,30 @@
-class Stack {
+class Queue {
   arr = [];
 
-  push(value) {
+  enque(value) {
     return this.arr.push(value);
   }
 
-  pop() {
-    return this.arr.pop();
-  }
+  deque() {
+    return this.arr.shift();
+  } //앞에 요소부터 제거
 
-  top() {
-    return this.arr.at(-1);
-  }
+  peek() {
+    return this.arr[0];
+  } //첫번째 요소
 
   get length() {
     return this.arr.length;
   }
 }
 
-const stack = new Stack();
-stack.push(1);
-stack.push(3);
-stack.push(5);
-stack.push(2);
-stack.push(4);
-console.log(stack.length);
-console.log(stack.top());
-console.log(stack);
+const queue = new Queue();
+queue.enque(1);
+queue.enque(3);
+queue.enque(5);
+queue.enque(2);
+queue.enque(4);
+console.log(queue.length);
+console.log(queue.peek());
+console.log(queue.deque());
+console.log(queue);
